36f7b17004c2ae605bb80611dd65a2c02f820751
AbhijitX Roy <abhijitx.roy@intel.com>
2021-04-23 14:48:27 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import btt_logo_img from "./images/btt-brand.png";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bug tracking Tools</h1>
+      <h4>More Code More Bug</h4>
+      <img src={btt_logo_img} alt="btt-logo" />
+    </div>
+  );
+}
+
+export default App;
